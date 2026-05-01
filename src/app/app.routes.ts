0fce86b5d21dad6bbb3ea950c0c580217b58049d
devamanin <amansinghpydev@gmail.com>
2026-05-01 15:22:29 +0530

    Added privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SplashComponent } from './components/splash/splash';
 import { HomeComponent } from './components/home/home';
 import { DeleteAccountComponent } from './components/delete-account/delete-account';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: SplashComponent },
   { path: 'home', component: HomeComponent },
   { path: 'delete-account', component: DeleteAccountComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'matching', redirectTo: 'home' },
   { path: 'video-chat', redirectTo: 'home' },
   { path: '**', redirectTo: '' }
