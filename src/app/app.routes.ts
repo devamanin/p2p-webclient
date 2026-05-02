b1c24b685fdc902cd24df59b9668ca1f4df41897
devamanin <amansinghpydev@gmail.com>
2026-05-02 09:22:43 +0530

    added child safety page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { SplashComponent } from './components/splash/splash';
 import { HomeComponent } from './components/home/home';
 import { DeleteAccountComponent } from './components/delete-account/delete-account';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy';
+import { SafetyStandardsComponent } from './components/safety-standards/safety-standards';
 
 export const routes: Routes = [
   { path: '', component: SplashComponent },
   { path: 'home', component: HomeComponent },
   { path: 'delete-account', component: DeleteAccountComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'safety-standards', component: SafetyStandardsComponent },
   { path: 'matching', redirectTo: 'home' },
   { path: 'video-chat', redirectTo: 'home' },
   { path: '**', redirectTo: '' }
